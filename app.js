3874a6bb33cd8a4751e09136a617b12d8325a165
Nick <30679225+nickcosmo@users.noreply.github.com>
2020-11-03 21:25:55 +0100

    include User model

--- app.js
+++ app.js
@@ -9,21 +9,42 @@ app.set('view-engine', 'ejs');
 app.set('views', 'views');
 
 const sequelize = require('./util/database.js');
+const Product = require('./models/product.js');
+const User = require('./models/user.js');
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const notFoundPage = require('./controllers/404.js');
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false })); // add 3rd party bodyparsing package
 app.use(express.static(path.join(__dirname, 'public'))); //serve static files
+
+app.use((req, res, next) => {
+    User.findByPk(1)
+        .then(user => {
+            req.user = user;
+            next();
+        }).catch(err => console(err))
+});
 
 app.use('/admin', adminData.routes);
 app.use(shopRoutes);
-
 app.use(notFoundPage.notFound);
+
+//define associations
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
+User.hasMany(Product);
 
 sequelize.sync()
     .then(result => {
+        return User.findByPk(1);
+        
+    }).then(user => {
+        if(!user) {
+            User.create({name: 'Nick', email: 'someemail@email.com'})
+        }
+        return user;
+    }).then(user => {
         app.listen(3000);
     })
     .catch(err => console.log(err));
--- models/product.js
+++ models/product.js
@@ -22,86 +22,9 @@ const Product = sequelize.define('product', {
         allowNull: false
     },
     imageUrl: {
-        type: Sequelize.STRING(16000),
+        type: Sequelize.BLOB,
         allowNull: false
     }
 });
 
 module.exports = Product;
-
-// const fs = require('fs');
-// const path = require('path');
-// const Cart = require('./cart');
-
-// const p = path.join(path.dirname(require.main.filename), 'data', 'products.json');
-
-// let getFileData = (cb) => {
-//     fs.readFile(p, (err, data) => {
-//         if (err) {
-//             cb([]);
-//         } else {
-//             if (data) {
-//                 cb(JSON.parse(data)); // need to parse data so it returns as an array!
-//             }
-//         }
-//     })
-// };
-
-// module.exports = class Product {
-//     constructor(title, imageUrl, desc, price) {
-//         this.title = title;
-//         this.imageUrl = imageUrl;
-//         this.desc = desc;
-//         this.price = price;
-//     }
-
-//     save() {
-//         // const p = path.join(path.dirname(process.mainModule.filename));
-//         this.id = Date.now().toString();
-//         getFileData(products => {
-//             products.push(this);
-//             fs.writeFile(p, JSON.stringify(products), (err) => {
-//                 if (err) { console.log(err) };
-//             });
-//         });
-//     }
-
-//     static edit(id, title, desc, price, imageUrl) {
-//         Product.fetchAll(products => {
-//             const product = products.find(p => p.id === id);
-//             product.id = id;
-//             product.title = title;
-//             product.desc = desc;
-//             product.price = price;
-//             product.imageUrl = imageUrl;
-//             fs.writeFile(p, JSON.stringify(products), (err) => {
-//                 if (err) { console.log(err) };
-//             });
-//         });
-//     }
-
-//     static delete(id) {
-//         Product.fetchAll(products => {
-//             const prodIndex = products.findIndex(p => p.id === id);
-//             const prodPrice = products[prodIndex].price;
-//             products.splice(prodIndex, 1);
-//             fs.writeFile(p, JSON.stringify(products), (err) => {
-//                 if(!err) {
-//                     Cart.deleteProduct(id, prodPrice);
-//                 }
-//                 if (err) { console.log(err) };
-//             });
-//         });
-//     }
-
-//     static fetchAll(cb) {
-//         getFileData(cb);
-//     }
-
-//     static findItem(id, cb) {
-//         getFileData(products => {
-//             const product = products.find(p => p.id === id);
-//             cb(product);
-//         });
-//     }
-// };
